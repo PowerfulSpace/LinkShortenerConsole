8ff767488a5ac3b3e545f2b39ec29874f9985e14
Alexander <sany199574@gmail.com>
2025-04-12 03:20:05 +0300

    Реазировал класс Program

--- src/PS.LinkShortener/Program.cs
+++ src/PS.LinkShortener/Program.cs
@@ -1,6 +1,65 @@
-﻿
+﻿using PS.LinkShortener.Services;
+using PS.LinkShortener.Storage;
 
 
 
-Console.ReadLine();
+string filePath = "Data/links.json";
+var storage = new JsonLinkStorage(filePath);
+var service = new LinkShortenerService(storage);
+
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("==== Link Shortener ====");
+    Console.WriteLine("1. Сократить ссылку");
+    Console.WriteLine("2. Развернуть короткую ссылку");
+    Console.WriteLine("0. Выйти");
+    Console.Write("Выбор: ");
+
+    var input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            Console.Write("Введите длинную ссылку: ");
+            var longUrl = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(longUrl))
+            {
+                Console.WriteLine("Ссылка не может быть пустой.");
+                break;
+            }
+
+            var shortUrl = service.Shorten(longUrl);
+            Console.WriteLine($"Короткая ссылка: {shortUrl}");
+            break;
+
+        case "2":
+            Console.Write("Введите короткую ссылку: ");
+            var inputShort = Console.ReadLine();
+            var original = service.Expand(inputShort);
+
+            if (original == null)
+            {
+                Console.WriteLine("Ссылка не найдена.");
+            }
+            else
+            {
+                Console.WriteLine($"Оригинальная ссылка: {original}");
+            }
+            break;
+
+        case "0":
+            return;
+
+        default:
+            Console.WriteLine("Некорректный выбор.");
+            break;
+    }
+
+}
+
+
+Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+Console.ReadKey();
 
