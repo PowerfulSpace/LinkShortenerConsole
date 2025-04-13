f5a48527f420b4a0bbfea721a702592938eb4c2f
Alexander <sany199574@gmail.com>
2025-04-13 05:46:36 +0300

    Добавлен класс  - RedisLinkStorage

---
+++ src/PS.LinkShortener/Storage/RedisLinkStorage.cs
@@ -0,0 +1,21 @@
+﻿
+namespace PS.LinkShortener.Storage
+{
+    public class RedisLinkStorage : ILinkStorage
+    {
+        public Dictionary<string, string> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string shortUrl, string longUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGet(string shortUrl, out string longUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
